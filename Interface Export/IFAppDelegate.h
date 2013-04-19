d7ab334cd9557123197208d8dadaa764b83b4a70
JustinRowe15 <JustinRowe15@fullsail.edu>
2013-04-18 23:01:28 -0400

    Net Zero Live link for MUX 1304, Week 2.

---
+++ Interface Export/IFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IFAppDelegate.h
+//  Interface Export
+//
+//  Created by Ruitao Su on 5/02/12.
+//  Copyright (c) 2012 Less Code Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
